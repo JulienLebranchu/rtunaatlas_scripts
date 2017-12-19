eef76b17f1ad2006bbf2c1e951a4e6998db803ae
ptaconet <paul.taconet@ird.fr>
2017-12-19 10:55:51 +0100

    change namùe

--- sardara_world/transform_trfmos_data_structure/metadata_source_datasets_to_database/compute_metadata.R
+++ sardara_world/transform_trfmos_data_structure/metadata_source_datasets_to_database/compute_metadata.R
@@ -1,18 +1,18 @@
 cat("Generating dataset of metadata...\n")
 # Read metadata file
-if (typeof(path_to_metadata_file)=="character"){
-  metadata_file<-read.csv(path_to_metadata_file,stringsAsFactors = F)
+if (typeof(path_to_df_metadata)=="character"){
+  df_metadata<-read.csv(path_to_df_metadata,stringsAsFactors = F)
 } else {
-  metadata_file<-path_to_metadata_file
+  df_metadata<-path_to_df_metadata
 }
 
 ### Get dataset_time_start and dataset_time_end
-metadata_file$dataset_time_start<-as.character(min(as.Date(dataset$time_start)))
-metadata_file$dataset_time_end<-as.character(max(as.Date(dataset$time_end)))
+df_metadata$dataset_time_start<-as.character(min(as.Date(dataset$time_start)))
+df_metadata$dataset_time_end<-as.character(max(as.Date(dataset$time_end)))
 
 ### Get datasets of code lists to load the dataset in the DB
-df_codelists<-read.csv(metadata_file$source_dataset_path_csv_codelists)
+df_codelists<-read.csv(df_metadata$source_dataset_path_csv_codelists)
 
 # generate metadata file
-metadata_file<-rtunaatlas::generate_metadata(metadata_file,"raw_dataset")
+df_metadata<-rtunaatlas::generate_metadata(df_metadata,"raw_dataset")
 
