f6beecd5dff950c618e30c039962bc43e23d44c4
Paul Taconet <paul.taconet@gmail.com>
2018-02-11 20:08:18 +0100

    remove sardara france et sardara world folders

--- sardara_world/create_own_tuna_atlas/sourced_scripts/disaggregate_on_1deg_data_with_resolution_superior_to_1deg.R
+++
@@ -1,4 +0,0 @@
-cat("Disaggregating data that are defined on quadrants or areas superior to 1° quadrant resolution to corresponding 1° quadrant by dividing the georef_dataset equally on the overlappings 1° x 1° quadrants...\n")
-georef_dataset<-rtunaatlas::spatial_curation_downgrade_resolution(con,georef_dataset,1)
-georef_dataset<-georef_dataset$df
-cat("Disaggregating data that are defined on quadrants or areas superior to 1° quadrant resolution to corresponding 1° quadrant OK\n")
