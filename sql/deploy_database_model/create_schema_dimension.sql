08108308b36fbe5039108b201c84d203f454971c
Paul Taconet <paul.taconet@gmail.com>
2018-02-20 11:03:16 +0100

    add 'equal'

--- sql/deploy_database_model/create_schema_dimension.sql
+++ sql/deploy_database_model/create_schema_dimension.sql
@@ -48,7 +48,7 @@ GRANT ALL ON TABLE %dimension_name%.%dimension_name%_mapping TO %db_admin%;
 
 INSERT INTO %dimension_name%.%dimension_name%(id_%dimension_name%,codesource_%dimension_name%) VALUES (0,'UNK');
 
-INSERT INTO %dimension_name%.%dimension_name%_mapping(%dimension_name%_mapping_id_from,%dimension_name%_mapping_id_to,%dimension_name%_mapping_relation_type) VALUES (0,0,NULL);
+INSERT INTO %dimension_name%.%dimension_name%_mapping(%dimension_name%_mapping_id_from,%dimension_name%_mapping_id_to,%dimension_name%_mapping_relation_type) VALUES (0,0,'equal');
 
 
 CREATE VIEW %dimension_name%.%dimension_name%_labels AS 
