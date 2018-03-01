191120cba7fb44ee0fab1099c5c0b3f5205f9f6e
Paul Taconet <paul.taconet@gmail.com>
2018-03-01 20:34:55 +0100

    lower cases data identifiers

--- tunaatlas_world/create_own_tuna_atlas/sourced_scripts/retrieve_nominal_catch.R
+++ tunaatlas_world/create_own_tuna_atlas/sourced_scripts/retrieve_nominal_catch.R
@@ -6,14 +6,14 @@ include_rfmo<-c(include_IOTC,include_IATTC,include_WCPFC,include_CCSBT,include_I
 # There are 2 ICCAT datasets for nominal catch: one that provides the stratification by Sampling areas, and one that provides the stratification by Stock areas. For nominal catch, the user decides as input parameter which one he wants to keep.
 if (exists("iccat_nominal_catch_spatial_stratification")){
   if (iccat_nominal_catch_spatial_stratification=="sampling_area"){
-    iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasICCAT_level0__bySamplingArea"
+    iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasiccat_level0__bysamplingarea"
   } else if (iccat_nominal_catch_spatial_stratification=="stock_area"){
-    iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasICCAT_level0__byStockArea"
+    iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasiccat_level0__bystockarea"
   }
-} else { iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasICCAT_level0__bySamplingArea" }
+} else { iccat_nominal_catch_dataset_permanent_identifier<-"atlantic_ocean_nominal_catch_tunaatlasiccat_level0__bySamplingArea" }
 
 rfmo<-c("IOTC","IATTC","WCPFC","CCSBT","ICCAT")
-nominal_catch_datasets_permanent_identifiers<-c("indian_ocean_nominal_catch_tunaatlasIOTC_level0","east_pacific_ocean_nominal_catch_tunaatlasIATTC_level0","west_pacific_ocean_nominal_catch_tunaatlasWCPFC_level0","southern_hemisphere_oceans_nominal_catch_tunaatlasCCSBT_level0__byGear",iccat_nominal_catch_dataset_permanent_identifier)
+nominal_catch_datasets_permanent_identifiers<-c("indian_ocean_nominal_catch_tunaatlasiotc_level0","east_pacific_ocean_nominal_catch_tunaatlasiattc_level0","west_pacific_ocean_nominal_catch_tunaatlaswcpfc_level0","southern_hemisphere_oceans_nominal_catch_tunaatlasccsbt_level0__bygear",iccat_nominal_catch_dataset_permanent_identifier)
 nominal_catch_contact_originators<-c("fabio.fiorellato@iotc.org","nvogel@iattc.org","PeterW@spc.int","CMillar@ccsbt.org","carlos.palma@iccat.int")
 nominal_catch_datasets_permanent_identifiers_to_keep<-NULL
 for (i in 1:length(include_rfmo)){
