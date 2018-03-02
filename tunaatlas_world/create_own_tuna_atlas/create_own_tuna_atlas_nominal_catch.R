f696e4f6b28ac1cb294796462d6c4a9d02bdfd8a
Paul Taconet <paul.taconet@gmail.com>
2018-03-02 18:21:44 +0100

    bring corrections to adapt to new codes

--- tunaatlas_world/create_own_tuna_atlas/create_own_tuna_atlas_nominal_catch.R
+++ tunaatlas_world/create_own_tuna_atlas/create_own_tuna_atlas_nominal_catch.R
@@ -10,7 +10,7 @@
 # wps.in: id = datasets_year_release, type = string, title = Year of release of the datasets by the tRFMOs. First available year is 2017. Usually, datasets released in the year Y contain the time series from the beginning of the fisheries (e.g. 1950) to year Y-2 (included). For instance 2017 will extract the datasets released in 2017 and that cover the time series from 1950 to 2015 (included), value = 2017;
 # wps.in: id = iccat_nominal_catch_spatial_stratification, type = string, title = Concerns ICCAT Nominal catch data. Use only if parameter include_ICCAT is set to TRUE. ICCAT nominal catch datasets can be spatially stratified either by sampling areas or by stock areas. Which spatial stratification to select for the output dataset? sampling_area or stock_area. ,value = "sampling_area|stock_area"
 # wps.in: id = mapping_map_code_lists, type = string, title = Map code lists (gears, species, flags, schooltypes, catchtype)? When using multiple sources of data (i.e. multiple RFMOS), code lists used by the various tRFMOs might be different. They should therefore be mapped to single code lists in order to be able to compare the data. TRUE : map code lists. The url to the datasets providing the code list mappings to use must be set in the parameter mapping_source_mappings. See parameter mapping_source_mappings for more details. FALSE : do not map code lists. Output data will use input codes., value = "TRUE" ;
-# wps.in: id = mapping_csv_mapping_datasets_url, type = string, title = Use only if parameter mapping_map_code_lists is set to TRUE. Path to the CSV file containing the dimensions that must be mapped and the name of the mapping dataset for each dimension mapped. The mapping datasets must be available in Sardara database. An example of this CSV can be found here: https://goo.gl/2hA1sq. , value="NULL" ;
+# wps.in: id = mapping_csv_mapping_datasets_url, type = string, title = Use only if parameter mapping_map_code_lists is set to TRUE. Path to the CSV file containing the dimensions that must be mapped and the name of the mapping dataset for each dimension mapped. The mapping datasets must be available in Sardara database. An example of this CSV can be found here: https://goo.gl/2hA1sq. , value="http://data.d4science.org/ZWFMa3JJUHBXWk9NTXVPdFZhbU5BUFEyQnhUeWd1d3lHbWJQNStIS0N6Yz0" ;
 # wps.in: id = mapping_keep_src_code, type = string, title = Use only if parameter mapping_map_code_lists is set to TRUE. In case of code list mapping (mapping_map_code_lists==TRUE) keep source coding system column? TRUE : conserve in the output dataset both source and target coding systems columns. FALSE : conserve only target coding system. , value= "FALSE" ;
 # wps.in: id = SBF_data_rfmo_to_keep, type = string, title = Concerns Southern Bluefin Tuna (SBF) data. Use only if parameter include_CCSBT is set to TRUE. SBF tuna data do exist in both CCSBT data and the other tuna RFMOs data. Wich data should be kept? CCSBT : CCSBT data are kept for SBF. other_trfmos : data from the other TRFMOs are kept for SBF. NULL : Keep data from all the tRFMOs. Caution: with the option NULL, data in the overlapping zones are likely to be redundant., value = "CCSBT|other_trfmos|NULL";
 # wps.out: id = zip_namefile, type = text/zip, title = Outputs are 3 csv files: the dataset of georeferenced catches + a dataset of metadata (including informations on the computation, i.e. how the primary datasets were transformed by each correction) [TO DO] + a dataset providing the code lists used for each dimension (column) of the output dataset [TO DO]. All outputs and codes are compressed within a single zip file. ; 
@@ -65,7 +65,11 @@ metadata$lineage<-c(metadata$lineage,"All the datasets were merged")
 if (mapping_map_code_lists=="TRUE"){
   source(paste0(url_scripts_create_own_tuna_atlas,"map_code_lists.R"))
   cat("Mapping code lists...\n")
-  nominal_catch<-function_map_dataset_codelists(nominal_catch,mapping_dataset,mapping_keep_src_code)
+  nominal_catch<-function_map_code_lists("catch",mapping_csv_mapping_datasets_url,nominal_catch,mapping_keep_src_code)
+  metadata$description<-paste0(metadata$description,nominal_catch$description)
+  metadata$lineage<-c(metadata$lineage,nominal_catch$lineage)
+  metadata$supplemental_information<-paste0(metadata$supplemental_information,nominal_catch$supplemental_information)
+  nominal_catch<-nominal_catch$dataset
   cat("Mapping code lists OK\n")
   }
 
@@ -73,10 +77,12 @@ if (mapping_map_code_lists=="TRUE"){
 #### 9) Southern Bluefin Tuna (SBF): SBF data: keep data from CCSBT or data from the other tuna RFMOs?
 
 if (SBF_data_rfmo_to_keep!="NULL"){
-  df<-nominal_catch
   source(paste0(url_scripts_create_own_tuna_atlas,"SBF_data_rfmo_to_keep.R"))
-  nominal_catch<-df
-  rm(df)
+  nominal_catch<-function_SBF_data_rfmo_to_keep(SBF_data_rfmo_to_keep,nominal_catch)
+  metadata$description<-paste0(metadata$description,nominal_catch$description)
+  metadata$lineage<-c(metadata$lineage,nominal_catch$lineage)
+  nominal_catch<-nominal_catch$dataset
+  
 }
 
 
