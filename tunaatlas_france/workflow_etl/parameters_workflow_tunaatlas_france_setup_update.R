ef961328a61cc15fc8c88d59c0038b28aca6ddac
Paul Taconet <paul.taconet@gmail.com>
2018-04-19 09:48:34 +0200

    update with vms fact

--- tunaatlas_france/workflow_etl/parameters_workflow_tunaatlas_france_setup_update.R
+++ tunaatlas_france/workflow_etl/parameters_workflow_tunaatlas_france_setup_update.R
@@ -20,16 +20,13 @@ db_admin_password="***"
 ## db_read_name,dimensions,variables_and_associated_dimensions : fill-in only if deploy_database_model==TRUE
 db_read_name="tunaatlas_inv"
 db_dimensions="area,catchtype,unit,flag,gear,schooltype,sex,sizeclass,species,time,source,fadclass,program,settype,sizetype,ocean,vessel"
-db_variables_and_associated_dimensions="fad=fadclass,time,area,unit@catch=schooltype,species,time,area,gear,flag,catchtype,unit,ocean,program,vessel@effort=schooltype,time,area,gear,flag,unit,ocean,program,vessel,settype@catch_at_size=schooltype,species,time,area,gear,flag,catchtype,sex,unit,sizeclass,program,sizetype,ocean,vessel"
+db_variables_and_associated_dimensions="vms=flag,gear,vessel,ocean,time,area,unit@fad=fadclass,ocean,time,area,unit@catch=schooltype,species,time,area,gear,flag,catchtype,unit,ocean,program,vessel@effort=schooltype,time,area,gear,flag,unit,ocean,program,vessel,settype@catch_at_size=schooltype,species,time,area,gear,flag,catchtype,sex,unit,sizeclass,program,sizetype,ocean,vessel"
 
 ## metadata_and_parameterization_csv_codelists : fill-in only if load_codelists==TRUE
 metadata_and_parameterization_csv_codelists="https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_france/metadata_and_parameterization_files/metadata_codelists_2017.csv"
 
-## metadata_and_parameterization_csv_mappings : fill-in only if load_codelists_mappings==TRUE
-#metadata_and_parameterization_csv_mappings="https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_world/metadata_and_parameterization_files/metadata_mappings_2017.csv"
-
 ## metadata_and_parameterization_csv_primary_datasets : fill-in only if transform_and_load_primary_datasets==TRUE
-metadata_and_parameterization_csv_primary_datasets="https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_france/metadata_and_parameterization_files/metadata_and_parameterization_datasets_2017.csv"
+metadata_and_parameterization_csv_primary_datasets="https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_france/metadata_and_parameterization_files/metadata_and_parameterization_tuna_atlas_france_primary_datasets_2017.csv"
 
 
 
