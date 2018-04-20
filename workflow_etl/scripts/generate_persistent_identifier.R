e31594bb148371f719b251a4e482ae9e17ea7284
Paul Taconet <paul.taconet@gmail.com>
2018-04-20 16:00:19 +0200

    generalize function to any column

--- workflow_etl/scripts/generate_persistent_identifier.R
+++ workflow_etl/scripts/generate_persistent_identifier.R
@@ -1,15 +1,15 @@
-generate_persistent_identifier<-function(metadata_and_parameterization,additional_metadata){
+generate_persistent_identifier<-function(metadata_and_parameterization_column,additional_metadata){
   
-  words_to_replace<-gsub("[\\%\\%]", "", regmatches(metadata_and_parameterization$persistent_identifier, gregexpr("\\%.*?\\%", metadata_and_parameterization$persistent_identifier))[[1]])
+  words_to_replace<-gsub("[\\%\\%]", "", regmatches(metadata_and_parameterization_column, gregexpr("\\%.*?\\%", metadata_and_parameterization_column))[[1]])
   
   if (length(words_to_replace)>0){
     for (j in 1:length(words_to_replace)){
       if (words_to_replace[j] %in% names(additional_metadata)){
-        persistent_identifier<-gsub(paste0("%",words_to_replace[j],"%"),as.character(additional_metadata[words_to_replace[j]]),metadata_and_parameterization$persistent_identifier)
+        persistent_identifier<-gsub(paste0("%",words_to_replace[j],"%"),as.character(additional_metadata[words_to_replace[j]]),metadata_and_parameterization_column)
       }
     }
   } else {
-    persistent_identifier<-metadata_and_parameterization$persistent_identifier
+    persistent_identifier<-metadata_and_parameterization_column
   }
   
   return(persistent_identifier) 
