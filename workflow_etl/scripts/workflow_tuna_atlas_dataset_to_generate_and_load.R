f0bedee45b6cc9212f49a412a2e26503f02ce4d4
Paul Taconet <paul.taconet@gmail.com>
2018-04-20 17:05:04 +0200

    correction - for multiple datasets to load

--- workflow_etl/scripts/workflow_tuna_atlas_dataset_to_generate_and_load.R
+++ workflow_etl/scripts/workflow_tuna_atlas_dataset_to_generate_and_load.R
@@ -14,28 +14,29 @@
   }
   
   # One by one load the datasets with their metadata
-  for (n_dataset_to_load in 1:length(dataset_and_metadata$dataset)){
+  for (n_dataset_to_load in 2:length(dataset_and_metadata$dataset)){
   dataset<-dataset_and_metadata$dataset[[n_dataset_to_load]]
   additional_metadata<-dataset_and_metadata$additional_metadata[[n_dataset_to_load]]
+  metadata_and_parameterization_this_df=metadata_and_parameterization
   
   # Complete metadata with values available in additional_metadata
-  metadata_and_parameterization<-fill_missing_metadata(metadata_and_parameterization,additional_metadata)
+  metadata_and_parameterization_this_df<-fill_missing_metadata(metadata_and_parameterization_this_df,additional_metadata)
 
   # Generate tuna atlas identifier
-  metadata_and_parameterization$identifier<-generate_tuna_atlas_identifier(metadata_and_parameterization,dataset,year_tuna_atlas)
+  metadata_and_parameterization_this_df$identifier<-generate_tuna_atlas_identifier(metadata_and_parameterization_this_df,dataset,year_tuna_atlas)
   
   # Push R script of dataset generation to the server
-  metadata_and_parameterization<-push_R_script_to_server(metadata_and_parameterization,virtual_repository_with_R_files,vre_username,vre_token)
+  metadata_and_parameterization_this_df<-push_R_script_to_server(metadata_and_parameterization_this_df,virtual_repository_with_R_files,vre_username,vre_token)
   
   # In the metadata, add the date of generation of the dataset
-  metadata_and_parameterization$date<-paste0("publication=",Sys.Date(),";")
+  metadata_and_parameterization_this_df$date<-paste0("publication=",Sys.Date(),";")
   
   # Generate metadata
-  df_metadata<-rtunaatlas::generate_metadata(metadata_and_parameterization,dataset,additional_metadata=additional_metadata)
+  df_metadata<-rtunaatlas::generate_metadata(metadata_and_parameterization_this_df,dataset,additional_metadata=additional_metadata)
   
   # Open code list data frame. Either it is stored or it is generated in the dataset_and_metadata
-  if (!is.na(metadata_and_parameterization$path_to_codelists_used_in_dataset)){
-    df_codelists<-get_data_frame_code_lists(metadata_and_parameterization)
+  if (!is.na(metadata_and_parameterization_this_df$path_to_codelists_used_in_dataset)){
+    df_codelists<-get_data_frame_code_lists(metadata_and_parameterization_this_df)
   } else {
     df_codelists<-dataset_and_metadata$df_codelists 
   }
@@ -46,7 +47,7 @@
                                        df_metadata=df_metadata,
                                        df_codelists_input=df_codelists)
   
-  cat(paste0("End processing dataset ",metadata_and_parameterization$persistent_identifier,"\n"))
+  cat(paste0("End processing dataset ",metadata_and_parameterization_this_df$persistent_identifier,"\n"))
   
   }
   
