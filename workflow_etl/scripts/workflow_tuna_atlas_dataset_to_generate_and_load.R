c4c9a47b845261c717f17ab9df3d8cae14efbbea
Paul Taconet <paul.taconet@gmail.com>
2018-04-20 17:24:08 +0200

    add "generation" in the dates. generation is when the dataset has been generated (i.e. the date the script was run)

--- workflow_etl/scripts/workflow_tuna_atlas_dataset_to_generate_and_load.R
+++ workflow_etl/scripts/workflow_tuna_atlas_dataset_to_generate_and_load.R
@@ -14,7 +14,7 @@
   }
   
   # One by one load the datasets with their metadata
-  for (n_dataset_to_load in 2:length(dataset_and_metadata$dataset)){
+  for (n_dataset_to_load in 1:length(dataset_and_metadata$dataset)){
   dataset<-dataset_and_metadata$dataset[[n_dataset_to_load]]
   additional_metadata<-dataset_and_metadata$additional_metadata[[n_dataset_to_load]]
   metadata_and_parameterization_this_df=metadata_and_parameterization
@@ -29,7 +29,7 @@
   metadata_and_parameterization_this_df<-push_R_script_to_server(metadata_and_parameterization_this_df,virtual_repository_with_R_files,vre_username,vre_token)
   
   # In the metadata, add the date of generation of the dataset
-  metadata_and_parameterization_this_df$date<-paste0("publication=",Sys.Date(),";")
+  metadata_and_parameterization_this_df$date_generation<-as.character(Sys.Date())
   
   # Generate metadata
   df_metadata<-rtunaatlas::generate_metadata(metadata_and_parameterization_this_df,dataset,additional_metadata=additional_metadata)
@@ -41,12 +41,14 @@
     df_codelists<-dataset_and_metadata$df_codelists 
   }
   
+  cat("Loading the dataset and the metadata in the DB...\n")
   # Load dataset and metadata
     rtunaatlas::load_raw_dataset_in_db(con=con_admin,
                                        df_to_load=dataset,
                                        df_metadata=df_metadata,
                                        df_codelists_input=df_codelists)
-  
+    
+  cat("Loading the dataset and the metadata in the DB OK\n")
   cat(paste0("End processing dataset ",metadata_and_parameterization_this_df$persistent_identifier,"\n"))
   
   }
