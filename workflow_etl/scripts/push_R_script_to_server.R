1a74b986e02297b59b450e7f7c2c141a7b8d37d4
Paul Taconet <paul.taconet@gmail.com>
2018-05-18 10:09:24 +0200

    remove the '' when the parameter is NULL

--- workflow_etl/scripts/push_R_script_to_server.R
+++ workflow_etl/scripts/push_R_script_to_server.R
@@ -17,6 +17,7 @@ push_R_script_to_server<-function(metadata_and_parameterization,virtual_reposito
   for (i in 1:length(parameters_columns)){
     if (nchar(metadata_and_parameterization[,parameters_columns[i]])>0){
       parameters_with_values<-paste0(parameters_with_values,gsub("parameter_","",parameters_columns[i]),"<-'",metadata_and_parameterization[,parameters_columns[i]],"'\n")
+      parameters_with_values<-gsub("'NULL'","NULL",parameters_with_values)
     }
   }
   writeLines(c(paste0("### Parameters \n",parameters_with_values,"\n"), Lines), con = fConn) 
