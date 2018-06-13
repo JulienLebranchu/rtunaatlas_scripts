54e7fc29e02ea20a83164a50ef140bc5adb94d92
Paul Taconet <paul.taconet@gmail.com>
2018-06-13 11:18:00 +0200

    add ability to load raw_dataset (in addition to codelists and mappings)

--- workflow_etl/scripts/workflow_tuna_atlas_dataset_to_load.R
+++ workflow_etl/scripts/workflow_tuna_atlas_dataset_to_load.R
@@ -13,6 +13,11 @@ workflow_tuna_atlas_dataset_to_load<-function(con_parameters,metadata_and_parame
   # Generate metadata
   df_metadata<-rtunaatlas::generate_metadata(metadata_and_parameterization,dataset)
   
+  # Open code list data frame. Either it is stored or it is generated in the dataset_and_metadata
+  if (metadata_and_parameterization$dataset_type=="raw_dataset"){
+    df_codelists<-get_data_frame_code_lists(metadata_and_parameterization)
+  } 
+  
   # Connect to db with admin rights
   drv <- dbDriver("PostgreSQL")
   con_admin <- dbConnect(drv, dbname=con_parameters$db_name, user=con_parameters$db_admin_name, password=con_parameters$db_admin_password, host=con_parameters$db_host)
@@ -26,6 +31,11 @@ workflow_tuna_atlas_dataset_to_load<-function(con_parameters,metadata_and_parame
     rtunaatlas::load_mapping_in_db(con=con_admin,
                                     df_to_load=dataset,
                                     df_metadata=df_metadata)
+  } else if (metadata_and_parameterization$dataset_type=="raw_dataset"){
+    rtunaatlas::load_raw_dataset_in_db(con=con_admin,
+                                   df_to_load=dataset,
+                                   df_metadata=df_metadata,
+                                   df_codelists_input=df_codelists)
   }
   
   dbDisconnect(con_admin)
